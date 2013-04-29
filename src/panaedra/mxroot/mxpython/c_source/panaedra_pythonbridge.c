205a8b44392fa307652530651a013bc8dcfca979
panaedra_h7f_teameffort <panaedra-software@users.noreply.github.com>
2013-04-29 15:19:39 +0200

    _plan: python aix shared object, intermediate

--- src/panaedra/mxroot/mxpython/c_source/panaedra_pythonbridge.c
+++ src/panaedra/mxroot/mxpython/c_source/panaedra_pythonbridge.c
@@ -16,120 +16,128 @@ static void** pModules = 0;
 static int iMaxModules = 0;
 
 PyMODINIT_FUNC
-  QxPy_InitializeInterpreter(char *cPyExePathIP, int iMaxModulesIP)
+	QxPy_InitializeInterpreter(char *cPyExePathIP, int iMaxModulesIP)
 {
 
-  int i;
-  void *oPyObject = 0;
+	int i;
+	void *oPyObject = 0;
 
-  iMaxModules = iMaxModulesIP;
+	iMaxModules = iMaxModulesIP;
 
-  fprintf(stdout, "Initialize iMaxModulesIP: \"%i\"\n", iMaxModulesIP);
+	fprintf(stdout, "Initialize iMaxModulesIP: \"%i\"\n", iMaxModulesIP);
 
-  pModules = (void**)malloc(sizeof(void*) * iMaxModulesIP);
-  if (pModules == 0)
-  {
-    printf("ERROR: Out of memory\n");
-  }
-  else
-  {
-    for (i = 0 ; i < iMaxModulesIP ; i++)
-    {
-      pModules[i] = 0;
-    }
+	pModules = (void**)malloc(sizeof(void*) * iMaxModulesIP);
+	if (pModules == 0)
+	{
+		printf("ERROR: Out of memory\n");
+	}
+	else
+	{
+		for (i = 0 ; i < iMaxModulesIP ; i++)
+		{
+			pModules[i] = 0;
+		}
 
-    // This function should be called before Py_Initialize() is called for the first time, if at all
-    Py_SetProgramName(cPyExePathIP);
-    Py_Initialize();
-    oPyObject = (PyCodeObject*)Py_CompileString("oHoi = 'Boe'\nprint 'Hallo dit is de eerste'\n", "dummydummy1", Py_file_input);
-    fprintf(stdout, "Initialize py object pointer nr 1: \"%p\"\n", oPyObject);
-    pModules[0] = oPyObject;
-    oPyObject = (PyCodeObject*)Py_CompileString("oHoi = 'Ba'\nprint 'Hallo dit is de tweede'", "dummydummy2", Py_file_input);
-    fprintf(stdout, "Initialize py object pointer nr 2: \"%p\"\n", oPyObject);
-    pModules[1] = oPyObject;
-  }
+		// This function should be called before Py_Initialize() is called for the first time, if at all
+		Py_SetProgramName(cPyExePathIP);
+		Py_Initialize();
+		oPyObject = (PyCodeObject*)Py_CompileString("oHoi = 'Boe'\nprint 'Hallo dit is de eerste'\n", "dummydummy1", Py_file_input);
+		fprintf(stdout, "Initialize py object pointer nr 1: \"%p\"\n", oPyObject);
+		pModules[0] = oPyObject;
+		oPyObject = (PyCodeObject*)Py_CompileString("oHoi = 'Ba'\nprint 'Hallo dit is de tweede'", "dummydummy2", Py_file_input);
+		fprintf(stdout, "Initialize py object pointer nr 2: \"%p\"\n", oPyObject);
+		pModules[1] = oPyObject;
+	}
 }
 
 PyMODINIT_FUNC
-  QxPy_SetCompiledPyCode(int iPyObjectIP, char *cPyCodeIP)
+	QxPy_SetCompiledPyCode(int iPyObjectIP, char *cPyCodeIP)
 {  
-  fprintf(stdout, "Set py object pointer: \"%p\"\n", pModules[0]);
-  //pModules[iPyObjectIP] = ...;
+	fprintf(stdout, "Set py object pointer: \"%p\"\n", pModules[0]);
+	//pModules[iPyObjectIP] = ...;
 }
 
 PyMODINIT_FUNC
-  QxPy_RunCompiledPyCode(int iModuleIP)
+	QxPy_RunCompiledPyCode(int iModuleIP)
 {  
-  PyObject* main_module = 0;
-  PyObject* global_dict = 0; 
-  PyObject* local_dict = 0;
-  PyObject* pRet = 0;
+	PyObject* main_module = 0;
+	PyObject* global_dict = 0; 
+	PyObject* local_dict = 0;
+	PyObject* pRet = 0;
 
-  PyObject* pStrong = 0;
+	PyObject* pStrong = 0;
 
-  fprintf(stdout, "QxPy_RunCompiledPyCode: \"%i\"\n", iModuleIP);
-  fprintf(stdout, "Run py object pointer: \"%p\" \"%p\"\n", pModules[0], pModules[1]);
+	int i ;
 
-  main_module = PyImport_AddModule("__main__");
-  fprintf(stdout, "Run py object main module: \"%p\"\n", main_module);
-  global_dict = PyModule_GetDict(main_module);
-  local_dict = PyDict_New();
+	fprintf(stdout, "QxPy_RunCompiledPyCode: \"%i\"\n", iModuleIP);
+	fprintf(stdout, "Run py object pointer: \"%p\" \"%p\"\n", pModules[0], pModules[1]);
 
-  
-  //fprintf(stdout, "Run py object pointer: \"%p\"-\"%p\"\n", oPyObject, (void*)(*((void)oPyObjectIP)));
+	main_module = PyImport_AddModule("__main__");
+	fprintf(stdout, "Run py object main module: \"%p\"\n", main_module);
+	global_dict = PyModule_GetDict(main_module);
+	local_dict = PyDict_New();
 
-  pRet = PyEval_EvalCode((PyCodeObject*)pModules[0], global_dict, local_dict);
-  fprintf(stdout, "pRet object pointer: \"%p\"\n", ((void *)pRet));
-  if (pRet != 0)
-  {
-    pStrong = PyObject_Str((PyObject*)pRet);
-    PyObject_Print(pStrong, stdout, 0); // Should be 'None'
-    fprintf(stdout, "\n");
-    Py_DECREF(pRet);
-  }
-  pRet = PyEval_EvalCode((PyCodeObject*)pModules[1], global_dict, local_dict);
-  fprintf(stdout, "pRet object pointer: \"%p\"\n", ((void *)pRet));
-  if (pRet != 0)
-  {
-    pStrong = PyObject_Str((PyObject*)pRet);
-    PyObject_Print(pStrong, stdout, 0); // Should be 'None'
-    fprintf(stdout, "\n");
-    Py_DECREF(pRet);
-  }
 
-  //Py_DECREF(main_module);
-  Py_DECREF(global_dict);
-  Py_DECREF(local_dict);
+	//fprintf(stdout, "Run py object pointer: \"%p\"-\"%p\"\n", oPyObject, (void*)(*((void)oPyObjectIP)));
+	for (i = 0; i < 1; i++)
+	{
+		pRet = PyEval_EvalCode((PyCodeObject*)pModules[0], global_dict, local_dict);
+		fprintf(stdout, "pRet object pointer: \"%p\"\n", ((void *)pRet));
+		if (pRet != 0)
+		{
+			pStrong = PyObject_Str((PyObject*)pRet);
+			PyObject_Print(pStrong, stdout, 0); // Should be 'None'
+			fprintf(stdout, "\n");
+			Py_DECREF(pRet);
+		}
+		pRet = PyEval_EvalCode((PyCodeObject*)pModules[1], global_dict, local_dict);
+		fprintf(stdout, "pRet object pointer: \"%p\"\n", ((void *)pRet));
+		if (pRet != 0)
+		{
+			pStrong = PyObject_Str((PyObject*)pRet);
+			PyObject_Print(pStrong, stdout, 0); // Should be 'None'
+			fprintf(stdout, "\n");
+			Py_DECREF(pRet);
+		}
+	}
+	// Note: Py_DECREF(main_module) gave a Mem Violation in OE10.2B batch utf-8. We don't create it ourselves, so it's probably not meant to be decreased anyway. Plus we only need one instance of the main module.
+	Py_DECREF(global_dict);
+	Py_DECREF(local_dict);
 }
 
 PyMODINIT_FUNC
-  QxPy_FreeCompiledPyCode(int iPyObjectIP)
+	QxPy_FreeCompiledPyCode(int iPyObjectIP)
 {  
-  fprintf(stdout, "Del py object number: \"%i\"\n", iPyObjectIP);
-  //Py_DECREF(oPyObjectIP);
+	fprintf(stdout, "Del py object number: \"%i\"\n", iPyObjectIP);
+	if (iPyObjectIP <= iMaxModules && pModules[iPyObjectIP] != 0) 
+	{
+		Py_DECREF(pModules[iPyObjectIP]);
+		pModules[iPyObjectIP] = 0;
+	}
 }
 
 
 PyMODINIT_FUNC 
-  QxPy_FinalizeInterpreter()
+	QxPy_FinalizeInterpreter()
 {
 
-  int i = 0;
+	int i = 0;
 
-  Py_Finalize();
+	// Free malloc of pModules void pointer array
+	fprintf(stdout, "Freeing pModules: \"%p\" \"%p\"\n", pModules, pModules[0]);
+	for (i = 0 ; i < iMaxModules ; i++)
+	{
+		if (pModules[i] != 0) 
+		{
+			Py_DECREF(pModules[i]);
+			pModules[i] = 0;
+		}
+	}
 
-  // Free malloc of pModules void pointer array
-  fprintf(stdout, "Freeing pModules: \"%p\" \"%p\"\n", pModules, pModules[0]);
-    for (i = 0 ; i < iMaxModules ; i++)
-    {
-      if (pModules[i] != 0) 
-      {
-        Py_DECREF(pModules[i]);
-      }
-    }
+	free(pModules);
+	pModules = 0;
 
-  free(pModules);
-  pModules = 0;
+	Py_Finalize();
 
 }
 

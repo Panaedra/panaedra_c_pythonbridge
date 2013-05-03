0a749b40d0bd4dbd658b7caa166c0f1f4ea045f0
panaedra_h7f_teameffort <panaedra-software@users.noreply.github.com>
2013-05-03 09:08:18 +0200

    _plan: python aix shared object, feedback of python compile errors

--- src/panaedra/mxroot/mxpython/c_source/panaedra_pythonbridge.c
+++ src/panaedra/mxroot/mxpython/c_source/panaedra_pythonbridge.c
@@ -11,6 +11,8 @@
 #define __bool_true_false_are_defined   1
 #endif 
 
+#define MAXERRORLEN 10000 // cErrorOP parameters should be preallocated with this size
+
 //static void *oPyObject = 0;
 static void** pModules = 0;
 static int iMaxModules = 0;
@@ -18,20 +20,21 @@ static PyObject* oMainModule = 0;
 static PyObject* oGlobalDict = 0; 
 
 PyMODINIT_FUNC
-	QxPy_InitializeInterpreter(char *cPyExePathIP, int iMaxModulesIP)
+	QxPy_InitializeInterpreter(char *cPyExePathIP, int iMaxModulesIP, char *cErrorOP)
 {
 
 	int i;
-	//void *oPyObject = 0;
 
 	iMaxModules = iMaxModulesIP;
+	cErrorOP[0] = 0;
 
 	fprintf(stdout, "Initialize iMaxModulesIP: \"%i\"\n", iMaxModulesIP);
 
 	pModules = (void**)malloc(sizeof(void*) * iMaxModulesIP);
 	if (pModules == 0)
 	{
-		printf("ERROR: Out of memory\n");
+		char* cErr = "ERROR: Out of memory\n";
+		strncat(cErrorOP, cErr, strnlen(cErr,MAXERRORLEN - strlen(cErrorOP) - 1));
 	}
 	else
 	{
@@ -42,58 +45,88 @@ PyMODINIT_FUNC
 
 		// This function should be called before Py_Initialize() is called for the first time, if at all
 		Py_SetProgramName(cPyExePathIP);
+
+		// Initialize the Python interpreter
 		Py_Initialize();
 
-		//oPyObject = (PyCodeObject*)Py_CompileString("oHoi = 'Boe'\nprint 'Hallo dit is de eerste'\n", "dummydummy1", Py_file_input);
-		//fprintf(stdout, "Initialize py object pointer nr 1: \"%p\"\n", oPyObject);
-		//pModules[0] = oPyObject;
-		//oPyObject = (PyCodeObject*)Py_CompileString("oHoi = 'Ba'\nprint 'Hallo dit is de tweede'", "dummydummy2", Py_file_input);
-		//fprintf(stdout, "Initialize py object pointer nr 2: \"%p\"\n", oPyObject);
-		//pModules[1] = oPyObject;
 	}
 }
 
 PyMODINIT_FUNC
-	QxPy_SetCompiledPyCode(int iPyObjectIP, char *cPyCodeIP)
+	QxPy_SetCompiledPyCode(int iPyObjectIP, char *cPyIdIP, char *cPyCodeIP, char *cErrorOP)
 {  
 	void *oPyObject = 0;
 
-	oPyObject = (PyCodeObject*)Py_CompileString(cPyCodeIP, "dummydummy1", Py_file_input);
-	fprintf(stdout, "Initialize py object pointer: \"%p\" \"%s\"\n", oPyObject, cPyCodeIP);
-	pModules[iPyObjectIP] = oPyObject;
+	cErrorOP[0] = 0;
 
-	//fprintf(stdout, "Set py object pointer: \"%p\"\n", pModules[0]);
+	oPyObject = (PyCodeObject*)Py_CompileString(cPyCodeIP, cPyIdIP, Py_file_input);
+	if (oPyObject == 0)
+	{
+		char* cErr = "ERROR: Compile error, invalid python code.\t";
+		strncat(cErrorOP, cErr, strnlen(cErr,MAXERRORLEN - strlen(cErrorOP) - 1));
+		cErr = cPyIdIP;
+		strncat(cErrorOP, cErr, strnlen(cErr,MAXERRORLEN - strlen(cErrorOP) - 1));
+		cErr = ":\t";
+		strncat(cErrorOP, cErr, strnlen(cErr,MAXERRORLEN - strlen(cErrorOP) - 1));
+		{
+			PyObject *ptype, *pvalue, *ptraceback;
+			
+			PyErr_Fetch(&ptype, &pvalue, &ptraceback); // Note: This fetch resets the error flag
+            
+			// Store results in cErrorOP
+			cErr = PyString_AsString(PyObject_Str(ptype));
+			strcat(cErr,"\t");
+    		strncat(cErrorOP, cErr, strnlen(cErr,MAXERRORLEN - strlen(cErrorOP) - 1));
+
+			cErr = PyString_AsString(PyObject_Str(pvalue));
+			strcat(cErr,"\t");
+    		strncat(cErrorOP, cErr, strnlen(cErr,MAXERRORLEN - strlen(cErrorOP) - 1));
+
+			cErr = PyString_AsString(PyObject_Str(ptraceback));
+    		strncat(cErrorOP, cErr, strnlen(cErr,MAXERRORLEN - strlen(cErrorOP) - 1));
+			
+		}
+	}
+	else
+	{
+		fprintf(stdout, "Initialize py object pointer: \"%p\" \"%s\"\n", oPyObject, cPyCodeIP);
+		pModules[iPyObjectIP] = oPyObject;
+	}
+
 }
 
 PyMODINIT_FUNC
 	QxPy_RunCompiledPyCode(int iModuleIP)
 {  
-	PyObject* local_dict = 0;
-	PyObject* pRet = 0;
-
-	PyObject* pStrong = 0;
-
-	fprintf(stdout, "QxPy_RunCompiledPyCode\"%i\"\n", iModuleIP);
-	fprintf(stdout, "Run py object pointer: \"%p\" \"%p\"\n", pModules[0], pModules[1]);
-
-	if (oMainModule == 0) oMainModule = PyImport_AddModule("__main__");
-	if (oGlobalDict == 0) oGlobalDict = PyModule_GetDict(oMainModule);
-
-	fprintf(stdout, "Run py object main module: \"%p\"\n", oMainModule);
-	local_dict = PyDict_New();
-
-	//fprintf(stdout, "Run py object pointer: \"%p\"-\"%p\"\n", oPyObject, (void*)(*((void)oPyObjectIP)));
-
-	pRet = PyEval_EvalCode((PyCodeObject*)pModules[iModuleIP], oGlobalDict, local_dict);
-	fprintf(stdout, "pRet object pointer: \"%p\"\n", ((void *)pRet));
-	if (pRet != 0)
+	if (pModules[iModuleIP] != 0)
 	{
-		pStrong = PyObject_Str((PyObject*)pRet);
-		PyObject_Print(pStrong, stdout, 0); // Should be 'None'
-		fprintf(stdout, "\n");
-		Py_DECREF(pRet);
+		PyObject* local_dict = 0;
+		PyObject* pRet = 0;
+
+		PyObject* pStrong = 0;
+
+		fprintf(stdout, "QxPy_RunCompiledPyCode\"%i\"\n", iModuleIP);
+		fprintf(stdout, "Run py object pointer: \"%p\" \"%p\"\n", pModules[0], pModules[1]);
+
+		if (oMainModule == 0) oMainModule = PyImport_AddModule("__main__");
+		if (oGlobalDict == 0) oGlobalDict = PyModule_GetDict(oMainModule);
+
+		fprintf(stdout, "Run py object main module: \"%p\"\n", oMainModule);
+		local_dict = PyDict_New();
+
+		//fprintf(stdout, "Run py object pointer: \"%p\"-\"%p\"\n", oPyObject, (void*)(*((void)oPyObjectIP)));
+
+		pRet = PyEval_EvalCode((PyCodeObject*)pModules[iModuleIP], oGlobalDict, local_dict);
+		fprintf(stdout, "pRet object pointer: \"%p\"\n", ((void *)pRet));
+		if (pRet != 0)
+		{
+			pStrong = PyObject_Str((PyObject*)pRet);
+			PyObject_Print(pStrong, stdout, 0); // Should be 'None'
+			fprintf(stdout, "\n");
+			Py_DECREF(pRet);
+		}
+		Py_DECREF(local_dict);
 	}
-	Py_DECREF(local_dict);
 }
 
 PyMODINIT_FUNC

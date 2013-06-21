e6dc1453e06811926c8e6c4b308a52196ecb0379
panaedra_h7f_teameffort <panaedra-software@users.noreply.github.com>
2013-06-21 15:03:11 +0200

    _plan: [Python] python bridge. Fix sc_environment GetPid on aix. Unbuffered mode for python cDataOP call via memptr return value implemented.

--- src/panaedra/mxroot/mxpython/c_source/panaedra_pythonbridge.c
+++ src/panaedra/mxroot/mxpython/c_source/panaedra_pythonbridge.c
@@ -15,17 +15,35 @@
 #define MAXERRORLEN 10000 // cErrorOP parameters should be preallocated with this size
 #define QXPYDEBUG 0
 
+#ifndef _WIN32
+#if defined(__cplusplus)
+#  define PyMODINIT_PCHAR extern "C" __declspec(dllexport) char*
+#else /* __cplusplus */
+#  define PyMODINIT_PCHAR char*
+#endif /* __cplusplus */
+#else
+#if defined(__cplusplus)
+#  define PyMODINIT_PCHAR extern "C" __declspec(dllexport) char*
+#else /* __cplusplus */
+#  define PyMODINIT_PCHAR __declspec(dllexport) char*
+#endif /* __cplusplus */
+#endif /* _WIN32 */
+
 // See: http://www.di.unipi.it/~nids/docs/longjump_try_trow_catch.html
 #define TRY do{ jmp_buf ex_buf__; if( !setjmp(ex_buf__) ){
 #define CATCH } else {
 #define ETRY } }while(0)
 #define THROW longjmp(ex_buf__, 1)
 
+#define DATAOP_BUFFERED 1
+#define DATAOP_UNBUFFERED 2
+
 static void** pModules = 0;
 static int iMaxModules = 0;
 static PyObject* oMainModule = 0;
 static PyObject* oGlobalDict = 0; 
-
+static PyObject* oLastUnbuffered = 0; 
+static char* pNullString = "";
 
 PyMODINIT_FUNC
   QxPy_InitializeInterpreter(char *cPyExePathIP, int iMaxModulesIP, char *cErrorOP)
@@ -172,18 +190,35 @@ PyMODINIT_FUNC
 } // QxPy_SetCompiledPyCode
 
 
-PyMODINIT_FUNC
-  QxPy_RunCompiledPyCode(int iModuleIP, char *cDataIP, char *cDataOP, long long iDataOpLengthIP, char *cErrorOP)
+void
+  RunCompiledPyCodeImplement(int iModuleIP, char *cDataIP, int iDataOpModeIP, char **cDataOP, long long iDataOpLengthIP, char *cErrorOP)
 {  
   cErrorOP[0] = 0;
   cErrorOP[1] = 0; // For Utf-8 data
   cErrorOP[2] = 0;
   cErrorOP[3] = 0; // For Utf-16 data
 
-  cDataOP[0] = 0;
-  cDataOP[1] = 0; // For Utf-8 data
-  cDataOP[2] = 0;
-  cDataOP[3] = 0; // For Utf-16 data
+  if (iDataOpModeIP == DATAOP_UNBUFFERED)
+  {
+    // For exeptional flows, initialize unbuffered pointer to a static null string
+    *cDataOP = pNullString;
+  }
+  else
+  {
+    cDataOP[0] = 0;
+    cDataOP[1] = 0; // For Utf-8 data
+    cDataOP[2] = 0;
+    cDataOP[3] = 0; // For Utf-16 data
+  }
+
+  if (oLastUnbuffered != 0)
+  {
+    #if QXPYDEBUG
+    fprintf(stdout, "Del oLastUnbuffered: \"%p\"\n", oLastUnbuffered);
+    #endif
+    Py_DECREF(oLastUnbuffered);
+    oLastUnbuffered = 0;
+  }
 
   if (pModules[iModuleIP] != 0)
   {
@@ -213,7 +248,7 @@ PyMODINIT_FUNC
     oLocalDict = PyDict_New();
 
     pPyUnicodeDataIP = PyUnicode_FromString(cDataIP);
-    pPyUnicodeDataOP = PyUnicode_FromString(cDataOP);
+    pPyUnicodeDataOP = PyUnicode_FromString(*cDataOP);
 
     #if QXPYDEBUG
     fprintf(stdout, "cDataIP: \"%s\"\n", cDataIP);
@@ -259,7 +294,7 @@ PyMODINIT_FUNC
       fprintf(stdout, "\n");
       fprintf(stdout, "iPyDataLength: \"%zd\"\n", iPyDataLength);
       fprintf(stdout, "iRetData: \"%i\"\n", iRetData);
-      fprintf(stdout, "cDataOP: \"%s\"\n", cDataOP);
+      fprintf(stdout, "cDataOP: \"%s\"\n", *cDataOP);
       fprintf(stdout, "pOutOP: \"%s\"\n", pOutOP);
       #endif
 
@@ -273,18 +308,32 @@ PyMODINIT_FUNC
       }
       else
       {
-        // Transfer the string from the Python world to the DLL world.
-        // There is no way around this deep copy, because we want to switch owner of the data.
-        strncpy(cDataOP, pOutOP , (size_t)iDataOpLengthIP);
-
-        if (iDataOpLengthIP < iPyDataLength)
+        if (iDataOpModeIP != DATAOP_UNBUFFERED)
         {
-          char cErr[100]; 
-          sprintf(cErr, "Warning: Output parameter truncated. Python string length \"%zd\" is larger than user-supplied maximum length \"%lld\".\t", iPyDataLength, iDataOpLengthIP);
-          strncat(cErrorOP, cErr, strnlen(cErr,MAXERRORLEN - strlen(cErrorOP) - 1));
+          // Transfer the string from the Python world to the DLL world.
+          // If we want to switch the owner of the data we need this deep copy. 
+          // Otherwise, use the unbuffered alternative.
+          strncpy(*cDataOP, pOutOP , (size_t)iDataOpLengthIP);
+
+          if (iDataOpLengthIP < iPyDataLength)
+          {
+            char cErr[100]; 
+            sprintf(cErr, "Warning: Output parameter truncated. Python string length \"%zd\" is larger than user-supplied maximum length \"%lld\".\t", iPyDataLength, iDataOpLengthIP);
+            strncat(cErrorOP, cErr, strnlen(cErr,MAXERRORLEN - strlen(cErrorOP) - 1));
+          }
+        }
+        else
+        {
+          // Unbuffered. Increase reference counter by 1, hold python object until next call.
+          Py_IncRef(pPyUnicodeDataRet);
+          oLastUnbuffered = pPyUnicodeDataRet;
+          *cDataOP = pOutOP;
+          #if QXPYDEBUG
+          fprintf(stdout, "Unbuffered cDataOP: \"%s\"\n", *cDataOP);
+          fprintf(stdout, "Unbuffered &pOutOP: \"%p\"\n", &pOutOP);
+          #endif
         }
       }
-
       Py_DECREF(pRet);
     }
     Py_DECREF(oLocalDict);
@@ -296,7 +345,28 @@ PyMODINIT_FUNC
 
   }
 
+} // RunCompiledPyCodeImplement
+
+
+PyMODINIT_FUNC
+  QxPy_RunCompiledPyCode(int iModuleIP, char *cDataIP, char *cDataOP, long long iDataOpLengthIP, char *cErrorOP)
+{  
+  RunCompiledPyCodeImplement(iModuleIP, cDataIP, DATAOP_BUFFERED, &cDataOP, iDataOpLengthIP, cErrorOP);
 } // QxPy_RunCompiledPyCode
+
+
+// Note: The only way to return an uninitialized memptr in OpenEdge ABL is by a return parameter.
+PyMODINIT_PCHAR
+  QxPy_RunCompiledPyCodeUnbuffered(int iModuleIP, char *cDataIP, char *cErrorOP)
+{  
+  char *cDataOP;
+  // Essentially the same as QxPy_RunCompiledPyCode, but instead of a deep copy to cDataOP, a pointer to 
+  // the char buffer of the (kept-alive) python unicode object is returned.
+  // This buffer should not be modified in any way.
+  // The python object (and the underlying string buffer) are cleaned up at the very next call, or at system exit.
+  RunCompiledPyCodeImplement(iModuleIP, cDataIP, DATAOP_UNBUFFERED, &cDataOP, 0, cErrorOP);
+  return cDataOP;
+} // QxPy_RunCompiledPyCodeUnbuffered
 
 
 PyMODINIT_FUNC
@@ -325,6 +395,12 @@ PyMODINIT_FUNC
   #if QXPYDEBUG
   fprintf(stdout, "Freeing pModules: \"%p\" \"%p\"\n", pModules, pModules[0]);
   #endif
+
+  if (oLastUnbuffered != 0)
+  {
+    Py_DECREF(oLastUnbuffered);
+    oLastUnbuffered = 0;
+  }
 
   for (i = 0 ; i < iMaxModules ; i++)
   {
